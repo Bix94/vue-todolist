860728dae7a5f727599de2c11f060faf0e06406b
Giacomo <giacomobiffi94@gmail.com>
2021-01-28 18:04:39 +0100

    push pre allenamento

--- js/script.js
+++ js/script.js
@@ -8,6 +8,26 @@ var app = new Vue ({
       'The last of us2',
       'Mass Effect',
       'Overwatch'
-    ]
+    ],
+    // da popolare in seguito
+    gamesRemoved: []
+  },
+  methods:{
+    // pushare in rimossi
+    // index = premuto  /  1 = solo un elemento
+    removeGame(index){
+      this.gamesRemoved.push(this.games[index]);
+      this.games.splice(index,1);
+      console.log(this.gamesRemoved);
+    },
+    // pushare in giochi
+    restoreGame(index){
+      this.games.push(this.gamesRemoved[index]);
+      this.gamesRemoved.splice(index,1);
+    },
+    // rimuovere defintivamente splice
+    deleteGame(index){
+      this.gamesRemoved.splice(index,1);
+    }
   }
 });
