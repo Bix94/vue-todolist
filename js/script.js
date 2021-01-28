12f182969c5dd3b4a4673a4d3f26c59da1549757
Giacomo <giacomobiffi94@gmail.com>
2021-01-28 16:37:33 +0100

    inizio es

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    games:
+    [
+      'Red Dead Redemption 2',
+      'Genshin Impact',
+      'The last of us2',
+      'Mass Effect',
+      'Overwatch'
+    ]
+  }
+});
