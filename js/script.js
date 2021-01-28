5b49c905459faa8c7ebec120f005e0d642601655
Giacomo <giacomobiffi94@gmail.com>
2021-01-29 00:05:30 +0100

    consegna es + bonus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue ({
   el: '#app',
   data: {
+    newGame: '',
     games:
     [
       'Red Dead Redemption 2',
@@ -13,14 +14,19 @@ var app = new Vue ({
     gamesRemoved: []
   },
   methods:{
+    // aggiungere gioco
+    addGame(){
+      this.games.push(this.newGame);
+      this.newGame = '';
+    },
     // pushare in rimossi
-    // index = premuto  /  1 = solo un elemento
     removeGame(index){
       this.gamesRemoved.push(this.games[index]);
+      // index = premuto  /  1 = solo un elemento
       this.games.splice(index,1);
       console.log(this.gamesRemoved);
     },
-    // pushare in giochi
+    // pushare in giochi (uno alla volta)
     restoreGame(index){
       this.games.push(this.gamesRemoved[index]);
       this.gamesRemoved.splice(index,1);
@@ -28,6 +34,26 @@ var app = new Vue ({
     // rimuovere defintivamente splice
     deleteGame(index){
       this.gamesRemoved.splice(index,1);
+    },
+    erase(index){
+      var domanda = confirm("Sei sicuro di voler svuotare il carrello?");
+      if (domanda === true){
+    // +++ PUSHA ARRAY INTERO CHIEDERE +++
+        this.gamesRemoved.push(this.games);
+        // svuoto tutto
+        this.games = [];
+      }else{
+        alert("Operazione annullata");
+      }
+    },
+    eraseCestino(index){
+      var domanda = confirm("Sei sicuro di voler svuotare il cestino?");
+      if (domanda === true) {
+        // svuoto tutto
+        this.gamesRemoved = [];
+      }else{
+        alert("Operazione annullata");
+      }
     }
   }
 });
